9f0814b1ab874d7bc305afd5414b511ede8ca29a
Jeffrey H. Johnson <trnsz@pobox.com>
2022-06-16 01:38:43 -0400

    Remove excess whitespace

--- src/g.c
+++ src/g.c
@@ -1604,10 +1604,10 @@ Hex digits missing or invalid";
 
 FSTR asc_tab[][4]
   = {
-  "NUL", "SOH",  "STX", "ETX",  "EOT", "ENQ", "ACK", "BEL",   "BS",  "HT",
-  "LF",   "VT",   "FF",  "CR",   "SO",  "SI", "DLE", "DC1",  "DC2", "DC3",
-  "DC4", "NAK",  "SYN", "ETB",  "CAN",  "EM", "SUB", "ESC",   "FS",  "GS",
-  "RS",   "US",  "DEL"
+  "NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL",  "BS",  "HT",
+  "LF",   "VT",  "FF",  "CR",  "SO",  "SI", "DLE", "DC1", "DC2", "DC3",
+  "DC4", "NAK", "SYN", "ETB", "CAN",  "EM", "SUB", "ESC",  "FS",  "GS",
+  "RS",   "US", "DEL"
   };
 
 /*
