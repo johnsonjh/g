93e561b3dab02086ff76032c526882c9a856ad43
Jeffrey H. Johnson <trnsz@pobox.com>
2022-06-19 06:52:15 -0400

    Bump version

--- src/g.c
+++ src/g.c
@@ -14,7 +14,7 @@
  */
 
 #undef VERSION_STRING
-#define VERSION_STRING      "4.7.4-dev (2022-06-17)"
+#define VERSION_STRING      "4.7.4-dev (2022-06-19)"
 
 #ifdef DOS
 # define UNIX               0
